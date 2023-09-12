17ec0e184fefdee22fb13bf7aeea2002d3b08f63
jabranemohamed <jabranemohamed@gmail.com>
2023-09-12 19:43:55 +0200

    update

--- src/main/java/org/mhj/employeemanager/config/CorsConfiguration.java
+++ src/main/java/org/mhj/employeemanager/config/CorsConfiguration.java
@@ -1,21 +1,26 @@
 /*
+
 package org.mhj.employeemanager.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@Component
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@Slf4j
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http:localhost:4200")
+                .allowedOrigins("http://localhost:4200","http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .maxAge(3600L)
                 .allowedHeaders("Origin", "Access-Control-Allow-Origin", "Content-Type",
@@ -26,5 +31,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
                         "Access-Control-Request-Method", "Access-Control-Request-Headers")
                 .allowCredentials(true);
     }
+
+
+
 }
+
 */
